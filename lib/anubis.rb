f2bcab20ffe2bfbde028edbce0e64f90a6ea9e39
Travis Dempsey <dempsey.travis@gmail.com>
2013-01-23 18:48:46 -0600

    added connect and configure functionality to top level module

--- lib/anubis.rb
+++ lib/anubis.rb
@@ -2,3 +2,27 @@ require 'thrift'
 
 require 'anubis/thrift'
 require 'anubis/client'
+require 'anubis/models'
+
+module Anubis
+
+  Connection = Client.new unless defined? Connection  
+  
+  class << self
+
+    def configure(&blk)
+      yield Connection if block_given?
+      Connection.reset_thrift_protocol!
+      self
+    end
+    
+    def list_tables
+      Connection.getTableNames
+    end
+    
+    def connect!
+      Connection.connect!
+    end
+
+  end
+end
