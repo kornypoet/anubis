3a57c7d3e4976819222dd886265f4fd1d356a4b0
Travis Dempsey <dempsey.travis@gmail.com>
2013-01-23 18:50:41 -0600

    client inherits from HBase::Client, supports basic open and close ruby style methods

--- lib/anubis/client.rb
+++ lib/anubis/client.rb
@@ -1,5 +1,79 @@
-module Anubis
-  class Client
+module Anubis  
+  class Client < Apache::Hadoop::Hbase::Thrift::Hbase::Client
+    attr_accessor :host, :port 
     
+    def initialize(options = {})
+      @host  = options[:host] || 'localhost'
+      @port  = options[:port] || 9090
+      @seqid = 0
+      reset_thrift_protocol!
+    end
+
+    def reset_thrift_protocol!
+      @socket         = Thrift::Socket.new(@host, @port)
+      @transport      = Thrift::BufferedTransport.new(@socket)
+      @iprot = @oprot = Thrift::BinaryProtocol.new(@transport)
+      self
+    end
+    
+    def connect!
+      @transport.open
+      true
+    end    
+    
+    def connected?
+      @transport.open?      
+    end
+
+    def disconnect!
+      @transport.close
+      true
+    end
+
+    def reconnect!
+      disconnect! if connected?
+      connect!
+    end
+
+    def to_s
+      "#<#{self.class}:#{object_id} host:#{@host.inspect} port:#{@port}>"
+    end
   end
 end
+
+# compact
+# majorCompact
+# getTableRegions
+# get
+# getVer
+# getVerTs
+# getRow
+# getRowWithColumns
+# getRowTs
+# getRowWithColumnsTs
+# getRows
+# getRowsWithColumns
+# getRowsTs
+# getRowsWithColumnsTs
+# mutateRow
+# mutateRowTs
+# mutateRows
+# mutateRowsTs
+# atomicIncrement
+# deleteAll
+# deleteAllTs
+# deleteAllRow
+# increment
+# incrementRows
+# deleteAllRowTs
+# scannerOpenWithScan
+# scannerOpen
+# scannerOpenWithStop
+# scannerOpenWithPrefix
+# scannerOpenTs
+# scannerOpenWithStopTs
+# scannerGet
+# scannerGetList
+# scannerClose
+# getRowOrBefore
+# getRegionInfo
