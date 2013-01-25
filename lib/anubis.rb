3302738ef31d21505169e769ecceb6a5c275f23f
Travis Dempsey <dempsey.travis@gmail.com>
2013-01-25 16:13:41 -0600

    elided column_group class; added top level get operation to Anubis module

--- lib/anubis.rb
+++ lib/anubis.rb
@@ -1,8 +1,10 @@
 require 'thrift'
+require 'gorillib/some'
 
 require 'anubis/thrift'
 require 'anubis/client'
 require 'anubis/models'
+require 'anubis/operations'
 
 module Anubis
 
@@ -24,5 +26,13 @@ module Anubis
       Connection.safely_send(:connect)
     end
 
+    def get(options = {})
+      Operation.new(options[:table]).
+        columns(options[:columns]).
+        qualifier(options[:qualifier]).
+        rows(options[:rows]).
+        get
+    end
+    
   end
 end
