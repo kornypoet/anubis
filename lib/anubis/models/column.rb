af318747e1e35faa1b2425e9c19dd78207ac37d6
Travis Dempsey <dempsey.travis@gmail.com>
2013-01-24 18:22:36 -0600

    added column group

--- lib/anubis/models/column.rb
+++ lib/anubis/models/column.rb
@@ -1,22 +1,23 @@
 module Anubis
   class Column < Apache::Hadoop::Hbase::Thrift::ColumnDescriptor
+
+    attr_accessor :qualifier, :table_name
     
-    def self.from_existing_columns table
-      columns = Connection.getColumnDescriptors table
-      columns.map do |key, col| 
-        new(
-            name:         col.name,
-            versions:     col.maxVersions, 
-            compression:  col.compression,
-            in_memory:    col.inMemory,
-            ttl:          col.timeToLive,
-            cached:       col.blockCacheEnabled,
-            bloom_filter: DefaultBloomFilter.new(col.bloomFilterType, col.bloomFilterVectorSize, col.bloomFilterNbHashes)
-            )
-      end
+    def self.from_existing(table, column)
+      new(
+          table_name:   table,
+          name:         column.name,
+          versions:     column.maxVersions, 
+          compression:  column.compression,
+          in_memory:    column.inMemory,
+          ttl:          column.timeToLive,
+          cached:       column.blockCacheEnabled,
+          bloom_filter: DefaultBloomFilter.new(column.bloomFilterType, column.bloomFilterVectorSize, column.bloomFilterNbHashes)
+          )     
     end
 
     def initialize(options = {})
+      @table_name                = options[:table_name]
       self.name                  = options[:name]
       self.maxVersions           = options[:versions]     || 3
       self.compression           = options[:compression]  || 'NONE'
@@ -27,6 +28,14 @@ module Anubis
       self.bloomFilterType       = @bloom_filter.type
       self.bloomFilterVectorSize = @bloom_filter.vector_size
       self.bloomFilterNbHashes   = @bloom_filter.hashes
+    end
+
+    def row(key = nil)
+      Row.new(table_name, key, [self.to_cell])
+    end
+
+    def to_cell
+      Cell.new(pretty_name, qualifier || '')
     end
 
     def pretty_name
@@ -70,5 +79,45 @@ module Anubis
         hashes:      hashes
       }
     end
+  end
+
+  class ColumnGroup
+    
+    attr_reader :table_name
+    
+    def self.from_existing table
+      existing = Connection.getColumnDescriptors table
+      columns  = existing.map{ |key, column| Column.from_existing(table, column) }
+      new(table, *columns)
+    end
+
+    def initialize(table, *columns)
+      @table_name = table
+      @group = columns.map{ |c| c.is_a?(Column) ? c : Column.new(name: c.to_s, table_name: table) }
+    end
+
+    def select_by_name(*names)
+      return self if names.empty?
+      columns = names.map{ |name| @group.detect{ |col| col.pretty_name == name.to_s } }.compact
+      self.class.new(table_name, *columns)
+    end
+
+    def to_a
+      @group
+    end
+
+    def to_s
+      @group.to_s
+    end
+
+    def qualifier(qual = nil)
+      @group.each{ |col| col.qualifier = qual.to_s }
+      self
+    end
+
+    def row key
+      Row.new(table_name, key, @group.map(&:to_cell))
+    end
+    
   end
 end
