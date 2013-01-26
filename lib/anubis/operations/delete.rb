d2969a8c7c96a5e1c56c9739f48d284f8c1b4e56
Travis Dempsey <dempsey.travis@gmail.com>
2013-01-25 18:33:38 -0600

    work on deletes

--- lib/anubis/operations/delete.rb
+++ lib/anubis/operations/delete.rb
@@ -5,32 +5,21 @@ module Anubis
     #
     def delete
       self.extend Delete
-      self.perform
+      perform
     end
     
     module Delete
-    
-      attr_reader :table, :row_key, :columns
       
-      def self.from_row row
-        table   = row.table
-        row_key = row.key
-        columns = row.cells.first.fullname rescue nil
-        new(table, row_key, columns)
+      def validate
+        true
       end
-      
-      def initialize(table, row_key, columns)
-        @table   = table
-        @row_key = row_key      
-        @columns = columns
+
+      def execute
+        deletes = @row_keys.product(mapping)
+        Connection.safely_send(:deleteAll, @table, row_key, columns, {})
       end
-      
-      def perform
-        if columns
-          Connection.deleteAll(table, row_key, columns, {})
-        else
-          Connection.deleteAllRow(table, row_key, {})
-        end
+
+      def prepare_results
         true
       end
     end
