f7f819550661ed2838eb1c2c2adc7a54c1b5faf2
Travis Dempsey <dempsey.travis@gmail.com>
2013-01-28 10:47:05 -0600

    added ability to get versions

--- lib/anubis/operations/get.rb
+++ lib/anubis/operations/get.rb
@@ -3,20 +3,32 @@ module Anubis
     #
     # Mixin how to 'get'
     #
-    def get
+    def get(versions = nil)
       self.extend Get
+      set_versions versions
       perform
     end
 
     module Get
+
+      def set_versions amt
+        @get_versions = amt
+      end
 
       def validate
         true
       end
 
       def execute
-        columns  = mapping.empty? ? nil : mapping
-        @results = Connection.safely_send(:getRowsWithColumns, @table, @row_keys, columns, {})
+        if @get_versions && @get_versions.is_a?(Numeric)
+          versioned_gets = @row_keys.product(mapping)
+          @results = versioned_gets.map do |row_key, column|
+            Connection.safely_send(:getVer, @table, row_key, column, @get_versions, {})
+          end
+        else
+          columns  = mapping.empty? ? nil : mapping
+          @results = Connection.safely_send(:getRowsWithColumns, @table, @row_keys, columns, {})
+        end
       end
       
       def prepare_results
